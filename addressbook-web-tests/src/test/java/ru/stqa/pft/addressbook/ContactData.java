9c45ce5760d13386c5cb45e900b1a1fc3620ae82
lubovdudko <lubovdudko3@gmail.com>
2018-04-16 00:53:01 +0300

    Выделены вспомогательные методы в ContactCreationTests.

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String title;
+  private final String address;
+  private final String phone;
+  private final String email;
+
+  public ContactData(String firstname, String lastname, String title, String address, String phone, String email) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.title = title;
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
