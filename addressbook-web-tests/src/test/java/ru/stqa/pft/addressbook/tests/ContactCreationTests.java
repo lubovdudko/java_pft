9b3d175da1a802b2d03c5e24f7890f3e510e73db
lubovdudko <lubovdudko3@gmail.com>
2018-04-17 23:00:15 +0300

    Созданы вспомогательные классы-помощники ContactHelper, GroupHelper, NavigationHelper.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,10 +7,10 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    app.gotoHomePage();
-    app.initContactCreation();
-    app.fillContactForm(new ContactData("first", "last", "title", "address", "123456", "abcde@gmail.com"));
-    app.submitContactCreation();
-    app.returnToHomePage();
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("first", "last", "title", "address", "123456", "abcde@gmail.com"));
+    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().returnToHomePage();
   }
 }
