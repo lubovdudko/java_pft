9f80bc5abff37f4c314a0c23850825e68655236d
lubovdudko <lubovdudko3@gmail.com>
2018-04-18 00:40:27 +0300

    Добавлен тест для модификации контакта ContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -39,4 +39,12 @@ public class ContactHelper extends HelperBase {
     click(By.xpath("//div/div[4]/form[2]/div[2]/input"));
     wd.switchTo().alert().accept();
   }
+
+  public void initContactModification() {
+    click(By.xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img"));
+  }
+
+  public void submitContactModification() {
+    click(By.name("update"));
+  }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase {
+
+  @Test
+  public void testContactModification () {
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillContactForm(new ContactData("first", "last", "title", "address", "123456", "abcde@gmail.com"));
+    app.getContactHelper().submitContactModification();
+    app.getContactHelper().returnToHomePage();
+  }
+}
