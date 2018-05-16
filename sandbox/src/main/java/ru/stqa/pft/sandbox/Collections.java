bf856f28bedca8025f54dda93224b0f4b028cfda
lubovdudko <lubovdudko3@gmail.com>
2018-05-17 00:25:01 +0300

    Создан новый класс Collections, содержащий коллекции.

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+  public static void main (String[] args){
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+
+    for (int i = 0; i < languages.size();i++) {
+      System.out.println("Я хочу выучить " + languages.get(i));
+    }
+  }
+}
