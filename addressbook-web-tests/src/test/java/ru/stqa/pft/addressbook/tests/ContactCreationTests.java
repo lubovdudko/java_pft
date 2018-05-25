2108491659d4ffa78f95230341a90e5d651236bf
lubovdudko <lubovdudko3@gmail.com>
2018-05-26 00:40:29 +0300

    Реализовано улучшение внешнего вида проверок с помощью библиотеки Hamcrest.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,29 +1,27 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
     app.goTo().HomePage();
-    Set<ContactData> before = app.contact().all();
+   Contacts before = app.contact().all();
     app.contact().initContactCreation();
     ContactData contact = new ContactData()
             .withFirstname("abs").withLastname("new").withTitle("title").withAddress("address").withPhone("123456").withEmail("abcde@gmail.com").withGroup("test1");
     app.contact().create(contact, true);
-    Set<ContactData> after= app.contact().all();
-    Assert.assertEquals(after.size(), before.size()+1);
+    Contacts after= app.contact().all();
+    assertThat(after.size(), equalTo(before.size()+1));
 
     contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
-    contact.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt());
-    before.add(contact);
-    Assert.assertEquals(before,after);
+    assertThat(after, equalTo(
+            before.withAdded(contact.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
   }
 }
