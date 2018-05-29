e2f058e3cdf017124039d3b6476eda5df6d4be85
lubovdudko <lubovdudko3@gmail.com>
2018-05-30 00:35:42 +0300

    Реализован генератор тестовых данных для создания контактов.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,4 +1,59 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContactDataGenerator {
+
+  @Parameter(names = "-c", description = "Contact count")
+  public int count;
+
+  @Parameter (names = "-f", description = "Target file")
+  public String file;
+
+  public static void main (String[] args) throws IOException {
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, new File(file));
+  }
+
+  private void save(List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts){
+      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),contact.getTitle(),
+              contact.getAddress(), contact.getHomePhone(), contact.getMobilePhone(),contact.getWorkPhone(), contact.getEmail(),contact.getAddress(),contact.getGroup()));
+    }
+    writer.close();
+  }
+
+  private List<ContactData> generateContacts(int count) {
+    List <ContactData> contacts = new ArrayList<ContactData>();
+    for (int i=0; i<count; i++){
+      contacts.add(new ContactData().withFirstname(String.format("firstName %s",i)).withLastname(String.format("lastName %s",i)).withTitle(String.format("title %s",i))
+              .withAddress(String.format("address %s",i)).withHomePhone(String.format("123456 %s",i)).withMobilePhone(String.format("22 2223 44 %s",i)).withWorkPhone(String.format("+37529153342%s",i)).withEmail(String.format("abc%s@gmail.com",i)).withGroup(String.format("group %s",i)));
+    }
+    return contacts;
+  }
 }
